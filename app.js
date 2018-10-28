724bf66c81a16b1ab9126e8bdbac10a4e120ce77
Egor Vinogradov <egor.vinogradov@corp.mail.ru>
2018-10-28 13:34:01 +0300

    Add search by raw chinese radical

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ function normalizeStr(str){
 }
 
 
+function isChineseCharacters(str) {
+  var re1 = new RegExp('^[\u4E00-\uFA29]*$');
+  var re2 = new RegExp('^[\uE7C7-\uE7F3]*$');
+  return re1.test(str) || re2.test(str);
+}
+
+
 function initSearch(){
   prepareSearchDict();
   prepareRadicalDict();
@@ -69,11 +76,21 @@ function getSearchMatches(keyword, words) {
 
 
 function getRadicalMatches(selectedRadicals, words) {
-  var radicalsArr = Object.keys(selectedRadicals);
+  var radicalsObj;
+  var radicalsArr;
+  if (typeof selectedRadicals === 'string') {
+    radicalsObj = {};
+    radicalsArr = selectedRadicals.split('');
+    radicalsArr.forEach(radical => radicalsObj[radical] = '');
+  }
+  else {
+    radicalsObj = selectedRadicals;
+    radicalsArr = Object.keys(selectedRadicals);
+  }
   if (radicalsArr.length) {
     return words.filter(word => {
       var matchesCount = 0;
-      for (var radical in selectedRadicals) {
+      for (var radical in radicalsObj) {
         if (word.radicals.indexOf(radical) > -1) {
           matchesCount++;
         }
@@ -145,6 +162,7 @@ function preventDefaultBrowserSearchInvocation(e){
 function hidePopups(){
   STORE.var({displayedRadicals: []});
   $$('.words__han.m-selected').map(item => item.classList.remove('m-selected'));
+  $$$('.info__block').hidden = true;
 }
 
 
@@ -195,18 +213,47 @@ function onRadicalItemHover(e) {
 }
 
 
+function onInfoIconClick() {
+  $$$('.info__block').hidden = false;
+}
+
+
+function onInfoTagClick(e) {
+  var text = e.target.innerText.trim();
+  $$$('.search__input').value = text;
+  performSearch(text);
+}
+
+
 function initEventBindings(){
   STORE.subscribe('searchParams', (searchParams) => {
     var selectedRadicals = STORE.var('selectedRadicals');
+    var {keyword, isRadical} = searchParams;
     var displayedWords;
     var displayedRadicals;
+    var displayedLessons;
 
-    if (searchParams.isRadical) {
-      displayedWords = getRadicalMatches(selectedRadicals, STORE.var('words'));
-      displayedRadicals = getSearchMatches(searchParams.keyword, STORE.var('radicals'));
+
+    if (isRadical) {
+      if (keyword && isChineseCharacters(keyword)) {
+        displayedWords = getRadicalMatches(keyword, STORE.var('words'));
+        displayedRadicals = [];
+      }
+
+      // TODO: add search by lesson
+      // else if (typeof +keyword === 'number' && +keyword >= 0) {
+      //   displayedWords // TODO: match by lesson
+      //   displayedRadicals = [];
+      //   displayedLessons = []; // TODO: lessons
+      // }
+
+      else {
+        displayedWords = getRadicalMatches(selectedRadicals, STORE.var('words'));
+        displayedRadicals = getSearchMatches(keyword, STORE.var('radicals'));
+      }
     }
     else {
-      displayedWords = getSearchMatches(searchParams.keyword, getRadicalMatches(selectedRadicals, STORE.var('words')));
+      displayedWords = getSearchMatches(keyword, getRadicalMatches(selectedRadicals, STORE.var('words')));
       displayedRadicals = [];
     }
     STORE.var({
@@ -248,6 +295,8 @@ function initEventBindings(){
   window.addEventListener('keyup', e => (e.which === 27 /* Esc */ ) && hidePopups() );
   window.addEventListener('keydown', preventDefaultBrowserSearchInvocation);
   $$$('.search__input').addEventListener('keydown', onSearchInputKeyup);
+  $$$('.info__icon').addEventListener('click', onInfoIconClick);
+  $$('.info__tag').map(item => item.addEventListener('click', onInfoTagClick));
 
 
   $$.delegate('.radicals__li', 'mouseover', onRadicalItemHover);
