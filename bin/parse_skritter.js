18594f0dfbe5be50d004b2c01ba6ec6d74f72797
Egor Vinogradov <egor.vinogradov@corp.mail.ru>
2018-10-31 21:29:55 +0300

    Add surge.sh redirect; edit parse Skritter script

--- bin/parse_skritter.js
+++ bin/parse_skritter.js
@@ -10,5 +10,11 @@ javascript:csvString = $('#vocablist-section-rows .row').toArray().map(row => {
   var chinese = $('.vocab-writing', row).eq(0).text().trim();
   var pinyin = $('.vocab-reading', row).text().trim().split(',')[0];
   var english = $('.vocab-definition', row).text().trim();
-  return `${chinese}, ${pinyin}, ${english}`;
-}).join('\n'); window.open(`https://majestic-geese.egorvinogradov.com/?text=${encodeURIComponent(csvString)}`);
+  return `${chinese},${pinyin},,${english}`;
+}).join('\n');
+
+chineseLetters = csvString.split('\n').map(str => str.split(',')[0]).join('\n');
+
+window.open(`https://translate.google.com/?source=osdd#zh-CN/ru/${encodeURIComponent(chineseLetters)}`);
+window.open('https://docs.google.com/spreadsheets/d/1HsUFYPAqch4DChO28lG--tgJ90BnqlxVKdOxYck1dBQ/edit#gid=0');
+window.open(`https://majestic-geese.egorvinogradov.com/?text=${encodeURIComponent(csvString)}`);
